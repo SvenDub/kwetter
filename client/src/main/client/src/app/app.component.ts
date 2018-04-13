94f728e76d5eb42103135360a4e952029880b057
Sven Dubbeld <s.dubbeld@student.fontys.nl>
2018-04-13 18:06:39 +0200

    Move login to fullscreen

--- client/src/main/client/src/app/app.component.ts
+++ client/src/main/client/src/app/app.component.ts
@@ -1,9 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {JwtHelperService} from '@auth0/angular-jwt';
+import {LoginService} from './api/login.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  username: string;
+  password: string;
+  loggedIn: boolean;
+
+  constructor(private loginService: LoginService, private jwtHelper: JwtHelperService) {
+  }
+
+  ngOnInit() {
+    this.loggedIn = !this.jwtHelper.isTokenExpired();
+  }
+
+  login() {
+    this.loginService.login(this.username, this.password)
+      .subscribe(resp => {
+        const authorizationHeader = resp.headers.get('Authorization');
+        const token = authorizationHeader.substr('Bearer'.length).trim();
+
+        localStorage.setItem('access_token', token);
+        window.location.reload();
+      });
+  }
 }
--- client/src/main/client/src/app/header/header.component.ts
+++ client/src/main/client/src/app/header/header.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {LoginService} from '../api/login.service';
 import {Router} from '@angular/router';
-import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-header',
@@ -10,31 +8,21 @@ import {JwtHelperService} from '@auth0/angular-jwt';
 })
 export class HeaderComponent implements OnInit {
 
-  username: string;
-  password: string;
   query: string;
-  loggedIn: boolean;
 
-  constructor(private loginService: LoginService, private router: Router, private jwtHelper: JwtHelperService) {
+  constructor(private router: Router) {
   }
 
   ngOnInit() {
-    this.loggedIn = !this.jwtHelper.isTokenExpired();
-  }
-
-  login() {
-    this.loginService.login(this.username, this.password)
-      .subscribe(resp => {
-        const authorizationHeader = resp.headers.get('Authorization');
-        const token = authorizationHeader.substr('Bearer'.length).trim();
-
-        localStorage.setItem('access_token', token);
-        window.location.reload();
-      });
   }
 
   onInput() {
     this.router.navigate(['/search/', {query: this.query}]);
   }
 
+  logout() {
+    localStorage.removeItem('access_token');
+    window.location.reload();
+    return false;
+  }
 }
