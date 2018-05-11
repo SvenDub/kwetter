c64b11087c62b0706ac0c77984ebc8a9dbc33cbf
Sven Dubbeld <s.dubbeld@student.fontys.nl>
2018-05-12 01:03:10 +0200

    Animate new tweets

--- client/src/main/client/src/app/timeline/timeline.component.ts
+++ client/src/main/client/src/app/timeline/timeline.component.ts
@@ -1,10 +1,21 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Tweet} from '../shared/models/tweet.model';
+import {animate, animateChild, query, stagger, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.css']
+  styleUrls: ['./timeline.component.css'],
+  animations: [
+    trigger('fade', [
+      transition(':enter', [style({opacity: 0, height: 0}), animate('.6s ease')])
+    ]),
+    trigger('stagger', [
+      transition(':enter', [
+        query(':enter', stagger('.1s', [animateChild()]))
+      ])
+    ])
+  ]
 })
 export class TimelineComponent implements OnInit {
 
