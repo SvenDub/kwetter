a5f585d85d280443629f70d82f8807676fe36727
Sven Dubbeld <s.dubbeld@student.fontys.nl>
2018-05-12 17:18:06 +0200

    Add e2e test for login

--- client/src/main/client/e2e/app.e2e-spec.ts
+++ client/src/main/client/e2e/app.e2e-spec.ts
@@ -1,14 +1,57 @@
 import { AppPage } from './app.po';
+import {browser, by, element, protractor} from 'protractor';
+import {Utils} from './utils';
 
 describe('client App', () => {
-  let page: AppPage;
+
+});
+
+describe('login page', () => {
+  let utils: Utils;
 
   beforeEach(() => {
-    page = new AppPage();
+    utils = new Utils();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  afterEach(() => {
+    utils.clearCredentials();
+  });
+
+  it('should show login form', () => {
+    browser.get('/');
+
+    expect(element(by.id('username')).isPresent()).toBe(true);
+    expect(element(by.id('password')).isPresent()).toBe(true);
+  });
+
+  it('should show signup form', () => {
+    browser.get('/');
+
+    expect(element(by.id('sign_up_email')).isPresent()).toBe(true);
+    expect(element(by.id('sign_up_name')).isPresent()).toBe(true);
+    expect(element(by.id('sign_up_username')).isPresent()).toBe(true);
+    expect(element(by.id('sign_up_password')).isPresent()).toBe(true);
+  });
+
+  it('should login', () => {
+    browser.get('/');
+
+    utils.login('SvenDub', 'password');
+
+    expect(element(by.id('username')).isPresent()).toBe(false);
+    expect(element(by.id('password')).isPresent()).toBe(false);
+    expect(utils.getLogoutButton().isPresent()).toBe(true);
+  });
+
+  it('should logout', () => {
+    browser.get('/');
+
+    utils.login('SvenDub', 'password');
+
+    utils.logout();
+
+    expect(element(by.id('username')).isPresent()).toBe(true);
+    expect(element(by.id('password')).isPresent()).toBe(true);
+    expect(utils.getLogoutButton().isPresent()).toBe(false);
   });
 });
